31ce028a3d42e41d7332c4324fbf7472ac4eda88
Conor Reisman <conorreisman@gmail.com>
2013-04-17 23:02:26 -0700

    Moved the solution into a project with wrapper main.

---
+++ 461projects/SolutionConsoleStart/src/SolutionConsoleStart.java
@@ -0,0 +1,13 @@
+import edu.uw.cs.cse461.consoleapps.infrastructure.ConsoleStart;
+
+
+public class SolutionConsoleStart {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ConsoleStart.main(args);
+	}
+
+}
