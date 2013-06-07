f338b3b6fccab07f5271a06606325437f5207f5c
Jackson Roberts <jcwr@uw.edu>
2013-06-06 23:15:27 -0700

    Finished a client-side RPC implementation.

--- 461projects/Net/src/edu/uw/cs/cse461/net/rpc/RPCCall.java
+++ 461projects/Net/src/edu/uw/cs/cse461/net/rpc/RPCCall.java
@@ -1,10 +1,13 @@
 package edu.uw.cs.cse461.net.rpc;
 
 import java.io.IOException;
+import java.net.Socket;
 import java.net.SocketException;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Map.Entry;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -13,6 +16,7 @@ import org.json.JSONObject;
 
 import edu.uw.cs.cse461.net.base.NetBase;
 import edu.uw.cs.cse461.net.base.NetLoadable.NetLoadableService;
+import edu.uw.cs.cse461.net.tcpmessagehandler.TCPMessageHandler;
 import edu.uw.cs.cse461.util.Log;
 
 /**
@@ -81,14 +85,40 @@ public class RPCCall extends NetLoadableService {
 
 	//-------------------------------------------------------------------------------------------
 	//-------------------------------------------------------------------------------------------
+
+	/**
+	 * Maps a host address to a cached socket to that host. This field is used
+	 * as a lock on the cache.
+	 */
+	private Map<HostAddress, RPCCallerSocket> socketCache;
+
+	/**
+	 * Maps a host address to the last time (in milliseconds) that socket was
+	 * used. Lock socketCache before modifying.
+	 */
+	private Map<HostAddress, Long> socketLastUsed;
+	
+	/**
+	 * A timer, which periodically evicts sockets from the cache
+	 */
+	private Timer timer;
 	
 	/**
 	 * The infrastructure requires a public constructor taking no arguments.  Plus, we need a constructor.
 	 */
 	public RPCCall() {
 		super("rpccall");
-	}
 
+		// Construct the socket cache
+		socketCache = new HashMap<HostAddress, RPCCallerSocket>();
+		socketLastUsed = new HashMap<HostAddress, Long>();
+		
+		// Start the cache evictor
+		int persistenceTimeout = NetBase.theNetBase().config().getAsInt("rpc.persistence.timeout", 30000);
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new CacheEvictor(persistenceTimeout), persistenceTimeout, persistenceTimeout);
+	}
+	
 	/**
 	 * This private method performs the actual invocation, including the management of persistent connections.
 	 * Note that because we may issue the call twice, we  may (a) cause it to be executed twice at the server(!),
@@ -114,7 +144,56 @@ public class RPCCall extends NetLoadableService {
 			int socketTimeout,        // max time to wait for reply
 			boolean tryAgain          // true if an invocation failure on a persistent connection should cause a re-try of the call, false to give up
 			) throws JSONException, IOException {
-		return null;
+		
+		RPCCallerSocket socket = getSocket(ip, port);
+		JSONObject result;
+		
+		try {
+			result = socket.invoke(serviceName, method, userRequest, socketTimeout);
+		} catch (IOException e) {
+			// If the previous invocation failed, re-try once with a new socket
+			if (tryAgain) {
+				synchronized (socketCache) {
+					socketCache.remove(new HostAddress(ip, port));
+				}
+				return _invoke(ip, port, serviceName, method, userRequest, socketTimeout, false);
+			} else {
+				throw e;
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * Returns a socket to the given host. Returns a cached socket if possible,
+	 * otherwise creates a new socket.
+	 * 
+	 * @param hostname
+	 *            the destination ip address
+	 * @param port
+	 *            the destination port
+	 * @return a socket to the specified host
+	 * @throws IOException
+	 *             if errors are encountered when communicating with the host
+	 * @throws JSONException
+	 *             if errors are encountered when parsing JSON messages to or
+	 *             from host
+	 */
+	private RPCCallerSocket getSocket(String hostname, int port) throws IOException, JSONException {
+		synchronized(socketCache) {
+			HostAddress key = new HostAddress(hostname, port);
+			if (socketCache.containsKey(key)) {
+				return socketCache.get(key);
+			}
+			RPCCallerSocket newSocket = new RPCCallerSocket(hostname, port, true);
+			
+			if (newSocket.isPersistent()) {
+				socketCache.put(key, newSocket);
+				socketLastUsed.put(key, System.currentTimeMillis());
+			}
+			
+			return newSocket;
+		}
 	}
 	
 	@Override
@@ -123,6 +202,89 @@ public class RPCCall extends NetLoadableService {
 	
 	@Override
 	public String dumpState() {
-		return "Current persistent connections are ...";
+		synchronized (socketCache) {
+			StringBuilder builder = new StringBuilder();
+			builder.append("Current persistent connections are:\n");
+			
+			for (HostAddress key : socketCache.keySet()) {
+				builder.append("    Socket '" + key + "' last used at " + socketLastUsed.get(key) + "\n");
+			}
+			
+			return builder.toString();
+		}
+	}
+	
+	/**
+	 * A HostAddress is a hostname/port pair. Used to uniquely identify sockets in the cache.
+	 */
+	private class HostAddress {
+		private String hostname;
+		private int port;
+		
+		public HostAddress(String hostname, int port) {
+			this.hostname = hostname;
+			this.port = port;
+		}
+		
+		@Override
+		public int hashCode() {
+			return hostname.hashCode() * port;
+		}
+		
+		@Override
+		public boolean equals(Object other) {
+			if (! (other instanceof HostAddress))
+				return false;
+			
+			HostAddress cast = (HostAddress) other;
+			return port == cast.port && hostname.equals(cast.hostname);
+		}
+		
+		@Override
+		public String toString() {
+			return hostname + ":" + port;
+		}
+	}
+	
+	/**
+	 * A task that periodically evicts old sockets
+	 */
+	private class CacheEvictor extends TimerTask {
+		private static final String TAG = "RPCCall:CacheEvictor";
+
+		private int evictionTime;
+		
+		public CacheEvictor(int evictionTime) {
+			this.evictionTime = evictionTime;
+		}
+		
+		/**
+		 * Removes any sockets older than evictionTime from the cache
+		 */
+		@Override
+		public void run() {
+			synchronized (socketCache) {
+				long now = System.currentTimeMillis();
+				Log.d(TAG, "Beginning cache eviction at " + now);
+				
+				// Iterate over the cached sockets, and remove those that were used more than
+				// evictionTime milliseconds ago
+				Iterator<Entry<HostAddress, Long>> iter = socketLastUsed.entrySet().iterator();
+				while (iter.hasNext()) {
+					Entry<HostAddress, Long> entry = iter.next();
+					HostAddress key = entry.getKey();
+					long lastUsed = entry.getValue();
+					
+					if (lastUsed + evictionTime < now) {
+						iter.remove();
+						socketCache.get(key).discard();
+						socketCache.remove(key);
+						Log.d(TAG, "Discarded socket '" + key + "' last used at " + lastUsed);
+					}
+				}
+				
+				Log.d(TAG, "Cache eviction complete");
+			}
+		}	
 	}
 }
--- 461projects/Net/src/edu/uw/cs/cse461/net/rpc/RPCCallerSocket.java
+++ 461projects/Net/src/edu/uw/cs/cse461/net/rpc/RPCCallerSocket.java
@@ -26,6 +26,16 @@ import edu.uw.cs.cse461.util.Log;
 	private static final String TAG = "RPCCallerSocket";	
 	
 	/**
+	 * The message handler used to communicate over this socket.
+	 */
+	private TCPMessageHandler messageHandler;
+	
+	/**
+	 * Whether this socket is intended to be persistent
+	 */
+	private boolean persistent;
+	
+	/**
 	 * Create a socket for sending RPC invocations, connecting it to the specified remote ip and port.
 	 * @param Remote host's name. In Project 3, it's not terribly meaningful - repeat the ip.
 	 *  In Project 4, it's intended to be the string name of the remote system, allowing a degree of sanity checking.
@@ -36,12 +46,100 @@ import edu.uw.cs.cse461.util.Log;
 	 * @throws JSONException
 	 */
 	RPCCallerSocket(String ip, int port, boolean wantPersistent) throws IOException, JSONException {
-		super(ip, port);	
+		super(ip, port);
+		
+		// Useful when debugging:
+		// Log.setLevel(Log.DebugLevel.DEBUG.toInt());
+		
+		// Create the message handler for this socket. Uses a default timeout, since the client has not had an
+		// opportunity to set one themselves
+		messageHandler = new TCPMessageHandler(this);
+		messageHandler.setTimeout(NetBase.theNetBase().config().getAsInt("net.timeout.socket", 2000));
+		messageHandler.setMaxReadLength(NetBase.theNetBase().config().getAsInt("tcpmessagehandler.maxmsglength", 2097148));
+		
+		// Handshake with the remote service
+		JSONObject options = new JSONObject();
+		if (wantPersistent)
+			options.put("connection", "keep-alive");
+		
+		RPCMessage connectMessage = new RPCControlMessage("connect", options);
+		Log.d(TAG, "Sending connection message");
+		messageHandler.sendMessage(connectMessage.marshall());
+		
+		// Read the server response
+		Log.d(TAG, "Awaiting connection response");
+		RPCMessage response = RPCMessage.unmarshall(messageHandler.readMessageAsString());
+		
+		if ("ERROR".equals(response.type())) {
+			// A server error occurred
+			String message = response.mObject.optString("message");
+			throw new IOException("RPC server error : " + message);
+		} else if (!"OK".equals(response.type())) {
+			// The type is incorrect
+			throw new IOException("RPC server sent incorrect type: " + response.type());
+		}
+		
+		// We must have received an OK message. Validate the message id.
+		RPCNormalResponseMessage okResponse = (RPCNormalResponseMessage) response;
+		
+		if (okResponse.callid() != connectMessage.id()) {
+			throw new IOException("RPC message id's do not match");
+		}
+		
+		// Determine whether the server  allows persistence
+		JSONObject value = okResponse.value();
+		if (wantPersistent && value.has("connection") && value.getString("connection").equals("keep-alive")) {
+			Log.d(TAG, "Server and client agree to use persistence");
+			persistent = true;
+		}
 	}
-
+	
+	synchronized public JSONObject invoke(String serviceName, String method, JSONObject userRequest, int timeout) 
+			throws IOException, JSONException {
+		messageHandler.setTimeout(timeout);
+		
+		// Send the invocation message
+		Log.d(TAG, "Sending RPC invocation");
+		RPCInvokeMessage invokeMessage = new RPCInvokeMessage(serviceName, method, userRequest);
+		messageHandler.sendMessage(invokeMessage.marshall());
+		
+		// Read a response from the server
+		Log.d(TAG, "Waiting for invocation response");
+		RPCMessage response = RPCMessage.unmarshall(messageHandler.readMessageAsString());
+		Log.d(TAG, "Invocation response received");
+		
+		if ("ERROR".equals(response.type())) {
+			// A server error occurred
+			String message = response.mObject.optString("message");
+			throw new IOException("RPC server error : " + message);
+		} else if (!"OK".equals(response.type())) {
+			// The type is incorrect
+			throw new IOException("RPC server sent incorrect type: " + response.type());
+		}
+		
+		// We must have received an OK message. Validate the message id.
+		RPCNormalResponseMessage okResponse = (RPCNormalResponseMessage) response;
+				
+		if (okResponse.callid() != invokeMessage.id()) {
+			throw new IOException("RPC message id's do not match");
+		}
+		
+		// The message is well-formed. Return the result
+		Log.d(TAG, "Invocation response is valid, returning to client");
+		return okResponse.value();
+	}
+	
+	/**
+	 * Returns whether this socket is persistent
+	 */
+	public boolean isPersistent() {
+		return persistent;
+	}
+	
 	/**
 	 * Close this socket.
 	 */
 	synchronized public void discard() {
+		messageHandler.close();
 	}
 }
